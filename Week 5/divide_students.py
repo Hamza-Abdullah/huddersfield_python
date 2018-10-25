c062e8601a670c7e80e5f83b4bad6578aec3ac9f
H.Abdullah U1874183 <U1874183@unimail.hud.ac.uk>
2018-10-25 16:55:18 +0100

    Week 5

---
+++ Week 5/divide_students.py
@@ -0,0 +1,18 @@
+students = 0
+num_of_pc = 0
+while students < 1:
+    try:
+        students = int(input('Enter the number of students there are: '))
+    except ValueError:
+        print('That value for number of students is not valid. Please try again.')
+while num_of_pc < 1:
+    try:
+        num_of_pc = int(input('Enter the number of PCs there are in each lab: '))
+    except ValueError:
+        print('That value for number of PCs is not valid. Please try again.')
+
+num_of_labs = str(students // num_of_pc + 1)
+if num_of_labs == '1':
+    print('You need 1 lab for all the students.')
+else:
+    print('You need ' + num_of_labs + ' labs for all the students.')
