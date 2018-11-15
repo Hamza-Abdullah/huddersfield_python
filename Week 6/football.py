15e5a44b1b1dba18c1b00f870c094f42d7bc09f7
hamza-abdullah <mrhamzaabdullah@gmail.com>
2018-11-15 17:00:43 +0000

    Week 6 football.

---
+++ Week 6/football.py
@@ -0,0 +1,22 @@
+team = input("Please enter your team name: ")
+wins = 0
+draws = 0
+losses = 0
+
+for count in range(5):
+    scored = int(input("Goals scored in football match #" + str(count + 1) + ": "))
+    against = int(input("Goals against in football match #" + str(count + 1) + ": "))
+    result = scored - against
+    if result > 0:
+        wins += 1
+    elif result < 0:
+        losses += 1
+    else:
+        draws += 1
+
+points = wins * 3 + draws
+print(team)
+print("Wins: " + str(wins))
+print("Draws: " + str(draws))
+print("Losses: " + str(losses))
+print("Points: " + str(points))
